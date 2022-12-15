61b61f8d12e36d1f7b91ca2cc502a88d5bfae6e6
Rally0078 <blackshotmedia123@gmail.com>
2022-12-15 16:55:38 +0530

    Reverted back changes that were Windows-specific

--- calendar.c
+++ calendar.c
@@ -7,11 +7,9 @@ int get_time_str(char *time_str)
 {
     time_t time_raw;
     time_raw = time(&time_raw);
-    struct tm local_time;
-    int r = localtime_s(&local_time, &time_raw);
-    int s = asctime_s(time_str, 30, &local_time);
-    if(r)
-        return r;
+    struct tm *local_time;
+    local_time = localtime(&time_raw);
+    int s = asctime_s(time_str, 30, local_time);
     if(s)
         return s;
     return 0;
@@ -21,30 +19,29 @@ int print_calendar()
 {
     time_t time_now;
     time_now = time(&time_now);
-    struct tm local_now;
-    int r = localtime_s(&local_now, &time_now);
-    if(r)
-        return r;
-    int mod_current_date = (local_now.tm_mday - 1) % 7;
-    int day_of_start = local_now.tm_wday - mod_current_date;
+    struct tm *local_now;
+    local_now = localtime(&time_now);
+    
+    int mod_current_date = (local_now->tm_mday - 1) % 7;
+    int day_of_start = local_now->tm_wday - mod_current_date;
 	if(day_of_start<0)
 		day_of_start = day_of_start+7;		//if day_of_start is negative, do magic modular arithmetic
     int crossed_flag = 0;
 
 	int month_days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
     int no_of_days;
-    if(local_now.tm_mon == 1)
+    if(local_now->tm_mon == 1)
     {
-            if((local_now.tm_year+1900)%400==0)
-                no_of_days = month_days[local_now.tm_mon]+1;
-            else if((local_now.tm_year+1900)%100 == 0)
-                no_of_days = month_days[local_now.tm_mon];
-            else if((local_now.tm_year+1900)%4 == 0)
-                no_of_days = month_days[local_now.tm_mon]+1;
+            if((local_now->tm_year+1900)%400==0)
+                no_of_days = month_days[local_now->tm_mon]+1;
+            else if((local_now->tm_year+1900)%100 == 0)
+                no_of_days = month_days[local_now->tm_mon];
+            else if((local_now->tm_year+1900)%4 == 0)
+                no_of_days = month_days[local_now->tm_mon]+1;
     }
-    no_of_days = month_days[local_now.tm_mon];
+    no_of_days = month_days[local_now->tm_mon];
     printf("\n");
-    printf("%19s %d\n", months[local_now.tm_mon], local_now.tm_year + 1900);
+    printf("%19s %d\n", months[local_now->tm_mon], local_now->tm_year + 1900);
     for (int i = 0; i < 39; i++)
         printf("=");
     printf("\n");
@@ -63,7 +60,7 @@ int print_calendar()
     for (int i = 1; i <= no_of_days; i++)
     {
 
-        if (i == local_now.tm_mday)
+        if (i == local_now->tm_mday)
         {
             if(i<=9)
                 printf("  | %d|", i);
